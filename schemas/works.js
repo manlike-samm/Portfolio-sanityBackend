91efa906aed3c6edec46abb4882ab62ef1abc47a
Samuel Ekanem <sd.ekanem@gmail.com>
2023-03-25 10:41:40 +0100

    First commit

---
+++ schemas/works.js
@@ -0,0 +1,52 @@
+export default {
+  name: "works",
+  title: "Works",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      title: "Title",
+      type: "string",
+    },
+
+    {
+      name: "description",
+      title: "Description",
+      type: "string",
+    },
+    {
+      name: "projectLink",
+      title: "Project Link",
+      type: "string",
+    },
+    {
+      name: "imgUrl",
+      title: "ImageUrl",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: "webImgUrl",
+      title: "WebImageUrl",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+    },
+
+    {
+      name: "tags",
+      title: "Tags",
+      type: "array",
+      of: [
+        {
+          name: "tag",
+          title: "Tag",
+          type: "string",
+        },
+      ],
+    },
+  ],
+};
